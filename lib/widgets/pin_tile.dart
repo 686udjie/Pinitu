4c6d8325ec5f700443192e30f3eacf281d52c1f1
686udjie <golonchy.aitta@gmail.com>
2025-11-29 17:06:58 +0200

    stupid add

--- lib/widgets/pin_tile.dart
+++ lib/widgets/pin_tile.dart
@@ -145,6 +145,7 @@ class _PinTileState extends State<PinTile> {
       content = CachedNetworkImage(
         imageUrl: widget.pin.mediaUrl,
         fit: BoxFit.fitWidth,
+        filterQuality: FilterQuality.high,
         httpHeaders: const {
           'User-Agent':
               'Mozilla/5.0 (iPhone; CPU iPhone OS 17_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.0 Mobile/15E148 Safari/604.1',
